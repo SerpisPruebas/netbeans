48ff71789cc1f422afcbbb8d69abe184beaa5145
angeles <angeles.chover@iesserpis.org>
2023-03-10 10:30:06 +0100

    primer commit

---
+++ src/main/java/test/ClienteEntidadClientes.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+import javax.persistence.*;
+import Dominio.Persona;
+import org.apache.logging.log4j.*;
+/**
+ *
+ * @author marmin
+ * @version 1.0
+ * esto es una prueba
+ */
+public class ClienteEntidadClientes {
+    static Logger log= LogManager.getRootLogger();
+    public static void main(String[] args) {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("ClientesPU");
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction tx=em.getTransaction();
+        
+        //iniciamos transaccion
+        tx.begin();
+        //no especifiqueis el id de la BD
+        Persona persona1 =new Persona("Antonio", "Pérez", "antonio@email.com");
+        log.debug("Objeto a persistir: " +persona1);
+        
+        //persistimos el objeto
+        em.persist(persona1);
+        
+        //terminar la transacción
+        tx.commit();
+        log.debug("Objeto persistido " +persona1);
+        em.close();
+        
+    }
+    
+}
